2b476d1d630f73ff49d987c2141fb71c0a2f7790
windowtrap <38334931+windowtrap@users.noreply.github.com>
2023-09-22 20:41:26 +1200

    Update verticalLines.js

--- MinDArT-2-Linescape/verticalLines.js
+++ MinDArT-2-Linescape/verticalLines.js
@@ -110,10 +110,10 @@ function setupArrays() {
     arr[i] = [];
     for (let j = 0; j < yCount; j++) {
       let _x = (width / xCount) * i;
-      _x = map(_x, 0, width, vW * -12, width + (vW * 15)); // ensures beyond margin
+      _x = map(_x, 0, width, vW * -15, width + (vW * 15)); // ensures beyond margin
       let _y = ((height / yCount) * j);
       _y = map(pow(_y, 2), 0, pow(height, 2), 0, height);
-      _y = map(_y, 0, height, vH * -3, height + (vH * 3)); // ensures beyond margin
+      _y = map(_y, 0, height, vH * -15, height + (vH * 15)); // ensures beyond margin
       arr[i][j] = createVector(_x, _y);
     }
   }
